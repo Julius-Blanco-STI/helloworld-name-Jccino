3c8b67134fd5c035c1687f1ca45526538fbbbcb7
Jccino <58575615+Jccino@users.noreply.github.com>
2019-12-06 09:15:07 +0800

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,22 @@
+
+package main;
+import java.util.*; 
+public class helloWorld {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+            System.out.println("Hello World");
+            System.out.println("What's your name?: ");
+            String a = scn.nextLine();
+            System.out.println("How old are you?: ");
+            int x = scn.nextInt();
+            System.out.println("What's your address?:" );
+            double y =scn.nextInt();
+            scn.nextLine();
+            String b = scn.nextLine();
+        
+        
+  }
+
+}
+    
+
